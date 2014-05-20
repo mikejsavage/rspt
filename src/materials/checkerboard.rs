d568e79854beac44ca2483abcf883db47b664efc
Michael Savage <mikejsavage@gmail.com>
2014-05-20 12:02:01 +0100

    Fix checkerboard material

--- src/materials/checkerboard.rs
+++ src/materials/checkerboard.rs
@@ -4,13 +4,13 @@ use maths::color::RGB;
 use materials::{ Material, BxDF };
 use materials::lambertian::Lambertian;
 
-pub struct Matte {
-	mat1 : ~Material,
-	mat2 : ~Material,
+pub struct Checkerboard {
+	mat1 : ~Material : Send + Share,
+	mat2 : ~Material : Send + Share,
 }
 
 impl Checkerboard {
-	pub fn new( mat1 : ~Material, mat2 : ~Material ) -> Checkerboard {
+	pub fn new( mat1 : ~Material : Send + Share, mat2 : ~Material : Send + Share ) -> Checkerboard {
 		return Checkerboard {
 			mat1 : mat1,
 			mat2 : mat2,
@@ -18,16 +18,16 @@ impl Checkerboard {
 	}
 }
 
-impl Material for Matte {
+impl Material for Checkerboard {
 	fn get_bxdf( &self, u : f64, v : f64 ) -> ~BxDF {
-		let u10 = u * 10.0 as i64;
-		let v10 = v * 10.0 as i64;
+		let u10 = ( u * 10.0 ).floor() as i64;
+		let v10 = ( v * 10.0 ).floor() as i64;
 
-		if u10 + v10 % 2 == 0 {
-			return mat1.get_bxdf( u, v );
+		if ( u10 + v10 ) % 2 == 0 {
+			return self.mat1.get_bxdf( u, v );
 		}
 		else {
-			return mat2.get_bxdf( u, v );
+			return self.mat2.get_bxdf( u, v );
 		}
 	}
 }
